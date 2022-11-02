53814ed752083d6fe8155f8267489900d14dd232
Maino24 <mg02.gabriele@gmail.com>
2022-11-02 23:05:42 +0100

    cambiamento colore casella in base alla bomba o no

--- main.js
+++ main.js
@@ -169,9 +169,12 @@ function creazioneQuadratino(num){
 
  const divQ = document.createElement(`div`);
  divQ.classList.add(`quadrato`);
- 
+ divQ.innerHTML = num
  if(array.includes(num) ){
-    divQ.classList.add(`quadratobomba`)
+    divQ.addEventListener(`click`, function(){
+      divQ.classList.add(`quadratobomba`)  
+    })
+    
  }else{
    
     
@@ -197,14 +200,17 @@ console.log(array)
 for(let i = 0; i < 100; i++){
         let elemento = creazioneQuadratino( i + 1)
         console.log(elemento);
+        let check =  array.includes(elemento)
         elemento.addEventListener(`click`, function(){
             console.log(this);
             this.classList.toggle(`attivazione`)
             
+          
+            
 
            })
     
-
+           
 
    
     griglia.append(elemento)
